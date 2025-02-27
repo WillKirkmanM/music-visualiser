7a640cef19bbcc92c53d120a6f9c82a8071ca3e7
Furkan5E <furkankeles10@hotmail.com>
2025-02-27 19:21:05 +0000

    Feat(sketch.js): music folder

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var sound = null;
 var fourier;
 
 function preload() {
-	sound = loadSound('assets/stomper_reggae_bit.mp3');
+	sound = loadSound('music/stomper_reggae_bit.mp3');
 	font = loadFont('assets/Arial.ttf');
 }
 
